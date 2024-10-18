ad1a0fba4d03a4b10e29adeb1084618b306da380
Prabhash Mac <gcpara8@gmail.com>
2024-10-17 19:11:21 -0500

    movieService framework final

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,3 +7,28 @@ import {} from "../lib/redisHelper.js"
 //setting up for redis cache
 const REDIS_KEY =''
 const REDIS_CACHE = ''
+
+
+export const getAllMovie = async(req, res)=>{
+    //since request -response style 
+    //so need to return res.code.message format
+}
+
+
+export const getMoviesByID = async (req, res) =>{
+
+}
+
+export const createMovie = async (req, res) =>{
+
+}
+
+export const updateMoviesByID = async (req, res) =>{
+
+
+}
+
+
+export const deleteMovieByID = async (req, res) =>{
+
+}
