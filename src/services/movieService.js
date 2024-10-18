10989d6250177d547e7b88da0f4051f1eb9d2446
Prabhash Mac <gcpara8@gmail.com>
2024-10-17 19:09:59 -0500

    movieService framework

---
+++ src/services/movieService.js
@@ -0,0 +1,9 @@
+
+//for to createmovieService
+import {} from "../repository/moviesRepository.js"
+import {} from "../lib/redisHelper.js"
+
+
+//setting up for redis cache
+const REDIS_KEY =''
+const REDIS_CACHE = ''
